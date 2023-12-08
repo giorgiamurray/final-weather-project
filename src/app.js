09a90de61ab46ec8f88aab45b486028730215229
giorgiamurray <150729719+giorgiamurray@users.noreply.github.com>
2023-12-08 15:01:01 +0000

    Added comma to js data

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ function refreshWeather(response) {
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
-  descriptionElement.innerHTML = response.data.condition.description;
+  descriptionElement.innerHTML = `, ${response.data.condition.description}`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   speedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
